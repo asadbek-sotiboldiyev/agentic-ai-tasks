8c96140496c65ef25f269deefb903d23e75396e0
asadbek-sotiboldiyev <ayebkovichas0209@gmail.com>
2026-02-20 17:07:12 +0500

    lesson-10

--- lesson-10/azure-ai.py
+++ lesson-10/azure-ai.py
@@ -1,20 +1,30 @@
-from openai import AzureOpenAI
 import os
 
-client = AzureOpenAI(
+from dotenv import load_dotenv
+from langchain_openai import AzureChatOpenAI
+from langchain_core.messages import HumanMessage, AIMessage
+
+load_dotenv()
+
+client = AzureChatOpenAI(
     api_key=os.getenv("AZURE_OPENAI_API_KEY"),
     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
     api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    deployment_name=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"),
 )
 
-history = [
-    {
-        "role":"user",
-        "content":"Hi"
-    }
-]
+
+history = []
 while True:
-    user_input = input("user: ")
+    user_input = input("-------\nuser: ")
     if user_input.lower() == "/bye":
         break
-    history.append()
+    history.append(HumanMessage(content=user_input))
+    response = client.invoke(history)
+    history.append(
+        AIMessage(content=response.content)
+    )
+    print("-----Response")
+    print(response)
+    print("-----Agent:\n")
+    print(response.content)
