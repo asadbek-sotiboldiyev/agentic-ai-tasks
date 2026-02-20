c99768a6bf760526c7d00d2a2edc8e55fbd74d97
asadbek-sotiboldiyev <ayebkovichas0209@gmail.com>
2026-02-20 17:19:00 +0500

    tested Gemini instead of AzureOpenAI with langchain

--- lesson-10/azure-ai.py
+++ lesson-10/azure-ai.py
@@ -2,15 +2,20 @@ import os
 
 from dotenv import load_dotenv
 from langchain_openai import AzureChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, AIMessage
 
 load_dotenv()
 
-client = AzureChatOpenAI(
-    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
-    deployment_name=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"),
+# client = AzureChatOpenAI(
+#     api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+#     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+#     api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+#     deployment_name=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"),
+# )
+client = ChatGoogleGenerativeAI(
+    api_key=os.getenv("GEMINI_API_KEY"),
+    model=os.getenv("GEMINI_MODEL_NAME"),
 )
 
 
